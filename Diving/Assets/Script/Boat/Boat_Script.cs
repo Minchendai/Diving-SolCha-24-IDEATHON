965c7c66fb488daf8af852300bbd4fe6c98d3a46
DMC52854 <dmc52854@gmail.com>
2023-12-28 18:18:40 +1100

    Barrier Finished

--- Diving/Assets/Script/Boat/Boat_Script.cs
+++ Diving/Assets/Script/Boat/Boat_Script.cs
@@ -6,13 +6,27 @@ public class Boat_Script : MonoBehaviour
 {
     public CharacterController player;
     public Rigidbody body;
+    public GameObject barrier_prefab;
     public float speed = 2f;
     public Vector3 roatation_speed = new Vector3(0, 100, 0);
+    public Material line_material;
+    private bool canPlaceBarrier = true;
+    private float distance;
+    private LineRenderer line;
+    
 
     private void Start()
     {
         player = GetComponent<CharacterController>();
         body = GetComponent<Rigidbody>();
+        line = gameObject.AddComponent<LineRenderer>();
+        // 设置材料的属性
+        line.material = line_material;
+        line.positionCount = 0; //　设置该线段由几个点组成
+
+        // 设置线段起点宽度和终点宽度
+        line.startWidth = 0.1f;
+        line.endWidth = 0.1f;
     }
     private void Update()
     {
@@ -22,6 +36,19 @@ public class Boat_Script : MonoBehaviour
         float y = move * System.MathF.Cos((body.rotation.eulerAngles.y - 90) * System.MathF.PI / 180);
         body.MoveRotation(body.rotation * deltaRotation);
         player.Move(new Vector3(x, 0, y));
+        distance -= Mathf.Sqrt(x * x + y * y);
+        if (distance <= 0)
+        {
+            canPlaceBarrier = true;
+        }
+        if (canPlaceBarrier)
+        {
+            GameObject.Instantiate(barrier_prefab, transform.position + new Vector3(0, 1, 0), body.transform.rotation * Quaternion.Euler(new Vector3(0, 90, 0)));
+            distance = 30;
+            canPlaceBarrier = false;
+            line.positionCount += 1;
+            line.SetPosition(line.positionCount - 1, transform.position + new Vector3(0, 3.3F, 0));
+        }
     }
 
 }
