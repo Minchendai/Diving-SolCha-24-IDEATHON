6bb403151456cbf2f5902e7a7ce8351f52ed34c1
Yiya Zhuang <yiyaz@student.unimelb.edu.au>
2023-12-29 18:50:33 +1100

    Oil shrinking

---
+++ Diving/Assets/Script/Oil/Oil Dynamic.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OilSpreading : MonoBehaviour
+{
+    public bool isShrinking = false;
+    public float spreadSpeed = 0.0001f;
+    public float shrinkSpeed = 0.0001f;
+    public float largestScale = 2f;
+    public float smallestScale = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isShrinking && transform.localScale.x<largestScale) {
+            // Oil spreading
+            Vector3 scaleChange = new Vector3(spreadSpeed, spreadSpeed, spreadSpeed);
+            transform.localScale += scaleChange;
+        } else if (isShrinking) {
+            if (transform.localScale.x>smallestScale) {
+                // Oil shrinking
+                Vector3 scaleChange = new Vector3(shrinkSpeed, shrinkSpeed, shrinkSpeed);
+                transform.localScale -= scaleChange;
+            } else {
+                // Oil invisible
+                gameObject.SetActive(false);
+            }
+        } else {
+            // Size unchange
+        }
+    }
+}
